8747e51211bf7ba648b7e3cff60f7252350ba8e3
LanceMuch7 <nathan.helmly@gmail.com>
2023-10-14 11:39:45 -0400

    now the files are added (note: don't trust commit -a to add new files)

---
+++ src/control-inputs.gd
@@ -0,0 +1,40 @@
+extends Node
+
+##		Class Variables			################################################
+@export var ScreenshotPath:String = OS.get_system_dir(OS.SYSTEM_DIR_PICTURES)
+
+
+##		Built-in Functions		################################################
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called when there is an input event
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
+	elif event.is_action_pressed("screenshot"):
+		var milis = Time.get_unix_time_from_system()
+		var d = Time.get_datetime_dict_from_unix_time(milis)
+		var texture = get_viewport().get_texture()
+		var image = texture.get_image()
+		var fn = "GWJ62_%d-%s-%d_%02d%02d%02d.png" % [d.year,d.month,d.day,d.hour,d.minute,d.second]
+		
+		image.save_png(ScreenshotPath + fn)
+	
+	if event.is_action("zoom_in"):
+		_zoom(-1)
+	elif event.is_action("zoom_out"):
+		_zoom(1)
+	elif event is InputEventMouseButton and event.pressed:
+		var colObj = null #cameraRay.get_collider()
+		if colObj and colObj.has_method("Interact"):
+			colObj.Interact(self)
+
+
+##		Public Functions		################################################
+
+
+##		Private Functions		################################################
+func _zoom(dir:float) -> void:
+	pass
