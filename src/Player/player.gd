766600b1ab19524f3236c702768bff6faf6e1b39
TJVN <ttolstrup@icloud.com>
2023-10-15 19:10:45 +0200

    Update
    Camera on the charachter and the Player switched from Node2D to KinematicBody

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -1,17 +1,14 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var speed = 80
-
-@onready var character_body = $CharacterBody2D
 
 func _ready():
 	pass
 
-
 func _physics_process(delta):
 	get_input(delta)
-	character_body.move_and_slide()
+	self.move_and_slide()
 
 func get_input(delta):
 	var input_direction = Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
-	character_body.velocity = input_direction * (speed * 100) * delta
+	self.velocity = input_direction * (speed * 100) * delta
