5efc4bb13551bf74adbb36d05c37ed17264efa48
TJVN <ttolstrup@icloud.com>
2023-10-15 16:53:08 +0200

    Tilemap updated

--- src/TileMaps.gd
+++ src/TileMaps.gd
@@ -1,13 +1,12 @@
 extends Node2D
 
 
-@onready var lpc_plant_repack = %lpc_plant_repack
-@onready var lpc_tile_atlas = %lpc_tile_atlas
+@onready var tilemap = %TileMap
 
 
 func _ready():
 	# example
-	# kenney_tiny_town.erase_cell(0, Vector2i(1, 1))
+	# tilemap.erase_cell(0, Vector2i(1, 1))
 	pass
 
 
