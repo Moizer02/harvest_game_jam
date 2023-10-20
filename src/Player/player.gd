499755d8b7e4f45b6b8618499db1e198e9af9f26
LanceMuch7 <nathan.helmly@gmail.com>
2023-10-20 15:36:14 -0400

    updates to player inventory, added static player glow

--- src/Player/player.gd
+++ src/Player/player.gd
@@ -13,6 +13,10 @@ func _ready():
 func _physics_process(delta):
 	movement_handler(delta)
 
+func _process(delta):
+	pass
+	#@TODO: Flashlight.angle = angle_to_point(mouse position - Flashlight position)
+
 func movement_handler(delta):
 	var input_direction = Input.get_vector("movement_left", "movement_right", "movement_up", "movement_down")
 	self.velocity = input_direction * (speed * 100) * delta
@@ -23,10 +27,30 @@ func movement_handler(delta):
 
 
 ## Give the player's inventory an item.
-## @param newItem - the item's string name.
+## @param itemName - the item's string name.
 ## @param amount - how many to add.
-func AddItem(newItem:String, amount:int=1):
-	if inventory.has(newItem):
-		inventory[newItem].val += amount
+## @return remainder (for things that have a max limit)
+func AddItem(itemName:String, amount:int=1) -> int:
+	var unused:int = 0
+	
+	if inventory.has(itemName):
+		if inventory[itemName].has("max"):
+			unused = mini(amount, inventory[itemName].max - inventory[itemName].val) - amount
+		inventory[itemName].val += amount - unused
 	else:
-		inventory[newItem] = {"val": amount}
+		inventory[itemName] = {"val": amount}
+	return unused
+
+## Remove an item from the player's inventory.
+## @param itemName - the item's string name.
+## @param amount - how many to take.
+## @return remainder (if the player had fewer than amount in inventory)
+func RemoveItem(itemName:String, amount:int=1) -> int:
+	var unused:int = 0
+	
+	if inventory.has(itemName):
+		unused = amount - mini(amount, inventory[itemName].val)
+		inventory[itemName].val -= amount + unused
+	else:
+		unused = amount
+	return unused
