35cdc4c360c9a7eb826e171c88a9467ab60d3cab
LanceMuch7 <nathan.helmly@gmail.com>
2023-10-22 13:20:51 -0400

    increase MaxStepsPerFrame to fix some jitters, added credits button to Market, added gameloop.gd to main game scene.

--- src/UI/market_ui.gd
+++ src/UI/market_ui.gd
@@ -1,9 +1,21 @@
 extends Panel
 
 var debounce:bool = false
+var harvested = 0
+
+
+func UpdateStock(item:String, value):
+	harvested = value
+	$Canopy/Stock.text = value
 
 
 func _onOpenPressed():
+	if debounce: return
+	debounce = true
 	$Cutscene.play("cutscene")
 	await $Cutscene.animation_finished
+	# @TODO: add text to the end that congratulates the player for winning/losing
 	get_tree().reload_current_scene()
+
+func _on_credits_pressed():
+	pass # Replace with function body.
--- src/gameloop.gd
+++ src/gameloop.gd
@@ -1,11 +1,25 @@
 extends Node2D
 
 const NEST = preload("res://src/bug_nest.tscn")
+const MINUTE:float = 60.0
+const HOUR:float = 3600.0
 
 @onready var bugNode = get_node_or_null("bugs")
 @onready var plantNode = get_node_or_null("plants")
 @onready var nestNode = get_node_or_null("nests")
 @onready var player = get_node_or_null("Player")
+var clock:float = 0
+var checkpoints = {
+	"Harvest3": false,
+}
+var forests = [
+	Vector2(816, -729),
+	Vector2(-1950, 25),
+	Vector2(-4556, -2344),
+]
+
+## Max distance away from the center of a forest that a BugNest can spawn.
+@export var nestRadius:int = 400
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,7 +28,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if checkpoints.Harvest3:
+		var nestCount = nestNode.get_child_count()
+		
+		clock += delta
+		if clock >= 15 * MINUTE and nestCount < 9:
+			_spawnNests(3)
+		elif clock >= 10 * MINUTE and nestCount < 6:
+			_spawnNests(3)
+		elif nestCount < 3:
+			_spawnNests(3)
 
 
 func _spawnNests(num:int=1):
@@ -23,3 +46,7 @@ func _spawnNests(num:int=1):
 		var newNest = NEST.instantiate()
 		
 		nestNode.add_child(newNest)
+		newNest.position = forests[randi_range(0, forests.size())] + Vector2(randi_range(-nestRadius, nestRadius), randi_range(-nestRadius, nestRadius))
+
+func _onPlayerInventoryUpdated(type, value):
+	$MarketUI.UpdateStock(type, value)
